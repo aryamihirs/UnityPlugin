ce3595f170145fd2dd94c691d1a612cf589bf7df
aryamihirsingh <arya.m.singh.10@gmail.com>
2017-03-07 19:17:55 +0530

    Files added

---
+++ Assets/cimg_img.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class cimg_img : MonoBehaviour
+{
+    [DllImport("ConsoleApplication13", EntryPoint = "img")]
+    public static extern void img();
+    // Use this for initialization
+    void Start()
+    {
+        img();
+    }
+}
